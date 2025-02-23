7d613fd617bc493651aae974f9179f3bcc291022
shubham <shubham@test.com>
2025-02-23 09:12:55 +0530

    initilize project

---
+++ src/test/java/com/singla/lld/visitor/VisitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singla.lld.visitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
